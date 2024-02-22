f0e0d51ce7d141e05b0f4164c3fbcb44bc113a59
Augusto <asergioscosta@gmail.com>
2024-02-22 20:55:47 -0300

    Resolvendo exercícios de sets

---
+++ Sets/exercicio1.py
@@ -0,0 +1,7 @@
+jogos = {'god of war', 'gta san andreas', 'counter strike', 'lol', 'fortnite'}
+
+
+letras_maiusculas = {jogo.upper()
+                     for jogo in jogos}
+
+print(letras_maiusculas)
---
+++ Sets/exercicio2.py
@@ -0,0 +1,8 @@
+meu_set = {1, 2, 3, 4, 1}
+meu_set_2 = set([1, 2, 8, 9, 10])
+
+
+print("União: ", meu_set |meu_set_2)
+print("Interseção: ", meu_set & meu_set_2)
+print("Diferença: ", meu_set - meu_set_2)
+print("Diferença Simétrica: ", meu_set ^ meu_set_2)
